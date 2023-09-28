3ec1dc9d838fa060aa1d97d8f9ac3a6d84246914
larcondo <larcondo90@gmail.com>
2023-09-28 17:15:11 -0300

    exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,18 @@ const dummy = (blogs) => {
   return Array.isArray(blogs) ? 1 : null
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  if (blogs.length === 1) return blogs[0].likes
+
+  return blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    0,
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
