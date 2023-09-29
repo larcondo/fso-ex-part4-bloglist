cf862fa4e31ff8bece784641051a5f7095054b76
larcondo <larcondo90@gmail.com>
2023-09-29 17:10:26 -0300

    exercise 4.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,21 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogRouter.post('/', async (request, response) => {
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0
+  })
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogRouter
---
+++ tests/blog_api.test.js
@@ -0,0 +1,113 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    "title": "Profit-focused systematic hierarchy",
+    "author": "Fernandina Rudolph",
+    "url": "https://admin.ch",
+    "likes": 82
+  },
+  {
+    "title": "Robust directional adapter",
+    "author": "Lonee Cumbes",
+    "url": "http://netvibes.com",
+    "likes": 24
+  },
+  {
+    "title": "Persistent 24/7 hardware",
+    "author": "Bunni Perrott",
+    "url": "https://mail.ru",
+    "likes": 66
+  },
+  {
+    "title": "Organized secondary open architecture",
+    "author": "Lory Gloy",
+    "url": "https://storify.com",
+    "likes": 57
+  },
+  {
+    "title": "Networked tangible moratorium",
+    "author": "Lona Junifer",
+    "url": "http://engadget.com",
+    "likes": 85
+  }, 
+  {
+    "title": "Realigned user-facing secured line",
+    "author": "Ruperta Matzke",
+    "url": "https://barnesandnoble.com",
+    "likes": 14
+  }, 
+  {
+    "title": "Customizable asymmetric core",
+    "author": "Emma Cornbill",
+    "url": "https://spotify.com",
+    "likes": 23
+  }, 
+  {
+    "title": "Triple-buffered bifurcated policy",
+    "author": "Illa Dalziell",
+    "url": "http://narod.ru",
+    "likes": 58
+  }, 
+  {
+    "title": "Mandatory upward-trending success",
+    "author": "Otho Sonnenschein",
+    "url": "http://ehow.com",
+    "likes": 25
+  }, 
+  {
+    "title": "Sharable logistical throughput",
+    "author": "Serena Housby",
+    "url": "http://newyorker.com",
+    "likes": 51
+  }
+]
+
+beforeEach( async () => {
+  await Blog.deleteMany({})
+  let blogObject = {}
+  for (const blog of initialBlogs) {
+    blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific blog title is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map( b => b.title)
+  expect(titles).toContain('Organized secondary open architecture')
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body.map( b => {
+    delete b.id
+    return b
+  })
+  expect(blogs).toContainEqual({
+    "title": "Triple-buffered bifurcated policy",
+    "author": "Illa Dalziell",
+    "url": "http://narod.ru",
+    "likes": 58
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
