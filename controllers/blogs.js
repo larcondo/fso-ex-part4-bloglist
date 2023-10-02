933d603ff933700130c6ba07e43158d5549e48d4
larcondo <larcondo90@gmail.com>
2023-10-02 17:33:50 -0300

    exercise 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { blogs: 0 })
@@ -9,24 +18,30 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
 
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'token invalid'
+    })
+  }
+
   if (!request.body.title) return response.status(400).json({ error: 'title is required'})
   if (!request.body.url) return response.status(400).json({ error: 'url is required'})
-
-  const users = await User.find({})
-  const blogUser = users.at(Math.random() * users.length)
+  
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
     likes: request.body.likes || 0,
-    user: blogUser.id
+    user: user.id
   })
 
   const savedBlog = await blog.save()
 
-  blogUser.blogs = blogUser.blogs.concat(savedBlog.id)
-  await blogUser.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
@@ -43,6 +58,12 @@ blogRouter.put('/:id', async (request, response) => {
 blogRouter.delete('/:id', async (request, response) => {
   const { id } = request.params
 
+  // delete blog id from user blogs array
+  await User.updateMany({}, { 
+    $pull: { 
+      blogs: { $in: [ id ] } 
+    }
+  })
   await Blog.findByIdAndDelete(id)
   response.status(204).end()
 })
