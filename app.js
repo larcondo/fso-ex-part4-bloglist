9089008ad6bee5bc03ba3774b9799e4db1d18402
larcondo <larcondo90@gmail.com>
2023-10-02 16:51:30 -0300

    exercise 4.18

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI)
 .then( () => {
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ controllers/login.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  if (!username) return response.status(401).json({ error: 'username required'})
+  if (!password) return response.status(401).json({ error: 'password required'})
+  
+  const user = await User.findOne({ username })
+  if (!user) return response.status(401).json({ error: 'username does not exist'})
+
+  const passwordCorrect = await bcrypt.compare(password, user.password)  
+  if (!passwordCorrect) return response.status(401).json({ error: 'password is incorrect' })
+
+  const userForToken = {
+    username: user.username,
+    id: user._id
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  response
+    .status(200)
+    .send({
+      token,
+      username: user.username,
+      name: user.name
+    })
+
+})
+
+module.exports = loginRouter
